a5f79bdc30968088c004b4f3f0906e143bdcf53f
Aljoaj <aljoaj9544@gmail.com>
2024-12-11 23:50:41 +0530

    first

---
+++ src/app/myservice.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface WebApiTab {
+  Id: number;
+  Name: string;
+  Age: number;
+  mark: number;
+}
+
+const endpoint = 'http://localhost:64855/api/API/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyserviceService {
+
+  constructor(private http: HttpClient) { }
+
+  addWebApiTab(student: any): Observable<any> {
+    return this.http.post(endpoint+'postwebapitab',student);
+  }
+  getAllWebApiTabs(): Observable<any> {
+    return this.http.get<WebApiTab>(endpoint+'getwebapitabs');
+    }
+    deleteWebApiTab(id: number): Observable<any> {
+      return this.http.delete<WebApiTab>(endpoint + 'deletewebapitab/'+id);
+    }
+    getWebApiTab(id: number): Observable<any> {
+      return this.http.get<WebApiTab>(endpoint + 'getwebapitab/' + id);
+    }
+    updateWebApiTab(id: number,product: WebApiTab):Observable<any> {
+      return this.http.put<WebApiTab>(endpoint + 'putwebapitab/' + id, product);
+    }
+}
